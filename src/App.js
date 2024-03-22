5e284cc5940f20676d8bb9daa9c6d9708450664f
CRS <christopher.r.simpson@gmail.com>
2024-03-22 07:41:16 -0400

    grid layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import GameGrid from "./Components/GameData/GameGrid";
@@ -8,22 +7,6 @@ function App() {
     <>
       <Header />
       <GameGrid />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
     </>
   );
 }
--- src/Components/GameData/GameGrid.js
+++ src/Components/GameData/GameGrid.js
@@ -1,45 +1,90 @@
 import React, { useState, useEffect } from "react";
 
 function GameGrid() {
+  const gridContainerStyle = {
+    display: "grid",
+    gridTemplateRows: "300px 300px 300px",
+    gridTemplateColumns: "525px 525px 525px",
+  };
+
+  const gridDivStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const girdImgStyle = {
+    display: "flex",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  };
+
   return (
     <>
-      <div>
-        <img
-          alt="willem"
-          src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
-        />
-        <img
-          alt="willem"
-          src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
-        />
-        <img
-          alt="willem"
-          src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
-        />
-        <img
-          alt="willem"
-          src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
-        />
-        <img
-          alt="willem"
-          src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
-        />
-        <img
-          alt="willem"
-          src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
-        />
-        <img
-          alt="willem"
-          src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
-        />
-        <img
-          alt="willem"
-          src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
-        />
-        <img
-          alt="willem"
-          src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
-        />
+      <div style={gridContainerStyle}>
+        <div style={gridDivStyle}>
+          <img
+            style={girdImgStyle}
+            alt="willem"
+            src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
+          />
+        </div>
+        <div style={gridDivStyle}>
+          <img
+            style={girdImgStyle}
+            alt="willem"
+            src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
+          />
+        </div>
+        <div style={gridDivStyle}>
+          <img
+            style={girdImgStyle}
+            alt="willem"
+            src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
+          />
+        </div>
+        <div style={gridDivStyle}>
+          <img
+            style={girdImgStyle}
+            alt="willem"
+            src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
+          />
+        </div>
+        <div style={gridDivStyle}>
+          <img
+            style={girdImgStyle}
+            alt="willem"
+            src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
+          />
+        </div>
+        <div style={gridDivStyle}>
+          <img
+            style={girdImgStyle}
+            alt="willem"
+            src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
+          />
+        </div>
+        <div style={gridDivStyle}>
+          <img
+            style={girdImgStyle}
+            alt="willem"
+            src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
+          />
+        </div>
+        <div style={gridDivStyle}>
+          <img
+            style={girdImgStyle}
+            alt="willem"
+            src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
+          />
+        </div>
+        <div style={gridDivStyle}>
+          <img
+            style={girdImgStyle}
+            alt="willem"
+            src="https://www.flickeringmyth.com/wp-content/uploads/2017/08/Florida-Project.png"
+          />
+        </div>
       </div>
     </>
   );
